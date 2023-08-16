315bd7e24a91102e63ab902a3722cd36c7a88e65
Gabriel <gabrielnazate124@gmail.com>
2023-08-16 18:07:38 -0500

    instalacion de cors

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const fileUpload = require('express-fileupload')
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors'); // Importa el paquete cors
 
-const app = express()
+const app = express();
 
-app.use(fileUpload())
+app.use(cors()); // Habilita el middleware cors
+app.use(fileUpload());
 
-app.post('/upload',(req,res) => {
-    let EDFile = req.files.file
-    EDFile.mv(`./files/${EDFile.name}`,err => {
-        if(err) return res.status(500).send({ message : err })
+app.post('/upload', (req, res) => {
+    let EDFile = req.files.file;
+    EDFile.mv(`./files/${EDFile.name}`, err => {
+        if (err) return res.status(500).send({ message: err });
 
-        return res.status(200).send({ message : 'File upload' })
-    })
-})
+        return res.status(200).send({ message: 'File upload' });
+    });
+});
 
-app.listen(3000,() => console.log('Corriendo'))
+app.listen(3000, () => console.log('Corriendo'));
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const fileInput = document.getElementById('fileInput');
 const fileInfo = document.getElementById('fileInfo');
 const preview = document.getElementById('preview');
+const progressBar = document.getElementById('progressBar');
 
 const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
-const maxFileSize = 5 * 1024 * 1024; // 5 MB in bytes
+const maxFileSize = 100 * 1024 * 1024; // 5 MB in bytes
 
 fileInput.addEventListener('change', function(event) {
   const selectedFile = event.target.files[0];
 
   if (selectedFile) {
     const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
-    
+
     if (allowedExtensions.includes('.' + fileExtension) && selectedFile.size <= maxFileSize) {
       fileInfo.innerHTML = `
         Nombre del archivo: ${selectedFile.name}<br>
@@ -30,6 +31,26 @@ fileInput.addEventListener('change', function(event) {
       } else {
         preview.style.display = 'none';
       }
+
+      // Upload the file using Axios
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      const config = {
+        onUploadProgress: progressEvent => {
+          const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+          progressBar.value = percentCompleted;
+        }
+      };
+
+      axios.post('http://localhost:3000/upload', formData, config)
+        .then(response => {
+          // Handle success if needed
+        })
+        .catch(error => {
+          // Handle error if needed
+        });
+      
     } else {
       fileInfo.innerHTML = 'Archivo no válido. Por favor, selecciona un archivo .jpg, .png o .gif que sea máximo 5 MB.';
       preview.style.display = 'none';
